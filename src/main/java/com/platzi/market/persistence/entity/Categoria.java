d686a5ac1ea493439672e146371cc2bfd3d0c5e0
yulio94 <me@julioestrada.dev>
2021-02-20 23:17:33 -0600

    feat: Database entities created.

---
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -0,0 +1,47 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "estado")
+    private Boolean estado;
+
+    @OneToMany(mappedBy = "category")
+    private List<Producto> productos;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String description) {
+        this.descripcion = description;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean status) {
+        this.estado = status;
+    }
+}
---
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -0,0 +1,93 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private Character medioPago;
+
+    private String comentario;
+    private Character estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "compra", cascade = {CascadeType.ALL})
+    private List<ComprasProducto> productos;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public Character getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(Character medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Character getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Character estado) {
+        this.estado = estado;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ComprasProducto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<ComprasProducto> productos) {
+        this.productos = productos;
+    }
+}
